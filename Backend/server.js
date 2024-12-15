9df45b3b4e3012587c89658286a42ef4d3e283db
rehman-m <302485736@student.rocmondriaan.nl>
2024-12-15 13:23:43 +0100

    Chat app

---
+++ Backend/server.js
@@ -0,0 +1,44 @@
+
+
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    // origin: "http://localhost:3000", // Adjust origin if needed
+    origin :"https://chat-app-type.netlify.app",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  // Listen for 'send_message' event
+  socket.on("send_message", (data) => {
+    console.log("Message from client:", data);
+
+    // Emit message to all connected clients
+    io.emit("receive_message", data);// Use the correct event name
+  });
+
+
+  
+
+  
+
+  // Handle client disconnection
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(8081, () => {
+  console.log("Server working on port 8081");
+});
+
+
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chat from './pages/Chat';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat/>
+      
+    </div>
+  );
+}
+
+export default App;
